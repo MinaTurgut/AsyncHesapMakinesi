cc3fb3845b05bd95433e2aea14c965ab689d20e5
MinaTurgut <minaturgut24@gmail.com>
2020-12-18 23:08:29 +0300

    Calculator with using async

---
+++ AsyncHesapMakinesi/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace AsyncHesapMakinesi
+{
+    class Program
+    {
+        public static async Task Hesapla(int num1, int num2, char islem)
+        {
+            if(islem == '+')
+            {
+                await Toplam(num1, num2);
+            }
+
+            if (islem == '-')
+            {
+                await Cıkarma(num1, num2);
+            }
+
+            if (islem == '*')
+            {
+                await Carpma(num1, num2);
+            }
+
+            if (islem == '/')
+            {
+                await Bolme(num1, num2);
+            }
+        }
+       
+
+        static async Task<int> Toplam(int sayi1, int sayi2)
+        {
+            int sonuc = await Task.Run(() => sayi1 + sayi2);
+            Console.WriteLine("İki sayının toplama sonucu: {0}", sonuc);
+            return sonuc;
+        }
+
+        static async Task<int> Cıkarma(int sayi1, int sayi2)
+        {
+            int sonuc = await Task.Run(() => sayi1 - sayi2);
+            Console.WriteLine("İki sayının çıkarma sonucu: {0}", sonuc);
+            return sonuc;
+        }
+
+        static async Task<int> Carpma(int sayi1, int sayi2)
+        {
+            int sonuc = await Task.Run(() => sayi1 * sayi2);
+            Console.WriteLine("İki sayının çarpma sonucu: {0}", sonuc);
+            return sonuc;
+        }
+
+        static async Task<int> Bolme(int sayi1, int sayi2)
+        {
+            int sonuc = await Task.Run(() => sayi1 / sayi2);
+            Console.WriteLine("İki sayının bölme sonucu: {0}", sonuc);
+            return sonuc;
+        }
+
+        static void Main(string[] args)
+        {
+            int sayii1 = 0;
+            int sayii2 = 0;
+            char islem;
+
+
+            while(true)
+            {
+                Console.WriteLine("Birinci sayıyı giriniz: ");
+                sayii1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("İkinci sayıyı giriniz: ");
+                sayii2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Operator tipini giriniz: ");
+                islem = char.Parse(Console.ReadLine());
+                Hesapla(sayii1, sayii2, islem);
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
